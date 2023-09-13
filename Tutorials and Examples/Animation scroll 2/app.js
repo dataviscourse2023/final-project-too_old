47fe60af481c0e23b443764b0b5e0fc57dfd23dd
Kevin <mooersk@gmail.com>
2023-09-13 10:17:54 -0600

    try some examples

---
+++ Tutorials and Examples/Animation scroll 2/app.js
@@ -0,0 +1,25 @@
+// Get the element to animate
+const element = document.querySelector('.my-element');
+
+// Define the options for the Intersection Observer
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5
+};
+
+// Create a new Intersection Observer
+const observer = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    // If element is in viewport, add the 'show' class to trigger the animation
+    if (entry.isIntersecting) {
+      element.classList.add('show');
+    }
+    else {
+      element.classList.remove('show');
+    }
+  });
+}, options);
+
+// Start observing the element
+observer.observe(element);
